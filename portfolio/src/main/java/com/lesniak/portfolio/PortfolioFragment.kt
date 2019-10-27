75e5d6105791f5c66487e4c0c872d1aa1c8a266a
Adrian Lesniak <adrian110288@gmail.com>
2019-10-27 17:57:39 +0000

    Add home, portfolio, about modules.

---
+++ portfolio/src/main/java/com/lesniak/portfolio/PortfolioFragment.kt
@@ -0,0 +1,32 @@
+package com.lesniak.portfolio
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+
+
+class PortfolioFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = PortfolioFragment()
+    }
+
+    private lateinit var viewModel: PortfolioViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_portfolio, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(PortfolioViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ portfolio/src/main/java/com/lesniak/portfolio/PortfolioViewModel.kt
@@ -0,0 +1,7 @@
+package com.lesniak.portfolio
+
+import androidx.lifecycle.ViewModel
+
+class PortfolioViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
