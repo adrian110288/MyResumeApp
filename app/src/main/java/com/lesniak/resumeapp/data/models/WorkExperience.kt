c301149aed90d90d98977807c176a56eece2bd49
Adrian Lesniak <adrian110288@gmail.com>
2019-11-02 02:04:53 +0000

    Add item layout for work experience.

--- app/src/main/java/com/lesniak/resumeapp/data/models/WorkExperience.kt
+++ app/src/main/java/com/lesniak/resumeapp/data/models/WorkExperience.kt
@@ -4,7 +4,7 @@ import com.google.gson.annotations.SerializedName
 import java.util.*
 
 data class WorkExperience(
-    val jobTitle: String,
+    val position: String,
     val company: String,
     @SerializedName("start_date") val startDate: Date,
     @SerializedName("end_date") val endDate: Date?,
